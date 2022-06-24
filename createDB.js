7f8e7a1af447189a11ea5e27af2337b1b3b63f36
MariyaBes <bessarabenko.m.i20@gmail.com>
2022-06-24 21:42:26 +0300

    данные из data в бд

--- createDB.js
+++ createDB.js
@@ -1,48 +1,38 @@
 var mongoose = require('mongoose')
+var data = require('./data.js').data
 mongoose.connect('mongodb://localhost:27017/all');
 
 var async = require('async');
 var Users = require("./models/hero").Users
 
-mongoose.connection.on("open", function(){
-  var db = mongoose.connection.db
-  db.dropDatabase(function(err){
-    if (err) throw err
-
-    async.parallel([
-      function(callback){
-        var groot = new Users({
-          nick:"groot"
-        })
-        groot.save(function(err, groot){
-          callback(err, "groot")
-        })
-      },
-      function(callback){
-        var star_lord = new Users({
-          nick:"star_lord"
-        })
-        star_lord.save(function(err, star_lord){
-          callback(err, "star_lord")
-        })
-      },
-      function(callback){
-        var rocket = new Users({
-          nick:"rocket"
-        })
-        rocket.save(function(err, rocket){
-          callback(err, "rocket")
-        })
-      }
-    ],
-    function(err,result){
-      if(err){
-        console.log(err)
-      }
-      else{
-        console.log("Успешно созданы герои с никами: " + result.join(", "))
-      }
-      mongoose.disconnect()
-    })
-  })
+async.series([
+  open,
+  dropDatabase,
+  createdUsers,
+  close
+],
+function(err, result){
+  if(err) throw err
+  console.log("oke")
 })
+
+function open(callback){
+  mongoose.connection.on("open", callback)
+}
+
+function dropDatabase(callback){
+  var db = mongoose.connection.db
+  db.dropDatabase(callback)
+}
+
+function createdUsers(callback){
+  async.each(data, function(usersData, callback){
+    var user = new mongoose.models.Users(usersData);
+    user.save(callback);
+  },
+  callback);
+};
+
+function close(callback){
+  mongoose.disconnect(callback)
+}
