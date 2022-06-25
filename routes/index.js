84a04d68f3b92d80add205bdd0ded007858bc352
MariyaBes <bessarabenko.m.i20@gmail.com>
2022-06-25 22:34:05 +0300

    cookie

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.cookie('Все супер!', 'Работает!!!!').render('index', {
+    req.session.all_work = "Работает!!"
+    res.render('index', {
       title: 'Сайт с описание героев из "Стражей Галактики"',
       menu: menu });
     })
