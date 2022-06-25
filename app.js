494a079c03ca29f2cebcc2f1b308ef95430e4a93
MariyaBes <bessarabenko.m.i20@gmail.com>
2022-06-25 22:25:23 +0300

    11.1-11.2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
+
+app.use(session({
+  secret: "I`mGroot",
+  cookie:{maxAge:60*1000}
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.render('index', { 
+    res.cookie('Все супер!', 'Работает!!!!').render('index', {
       title: 'Сайт с описание героев из "Стражей Галактики"',
       menu: menu });
     })
