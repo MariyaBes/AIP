aaddbf5e452963e2f272d9745c49540c6ffd1290
MariyaBes <bessarabenko.m.i20@gmail.com>
2022-06-26 15:29:52 +0300

    добавление пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var Hero = require("../models/hero").Hero
+var User = require('./../models/User').User
 var router = express.Router();
 
 /* GET home page. */
@@ -20,9 +21,27 @@ router.get("/logreg", function(req, res, next){
 
 
 /* Post log_in/Sing_in */
-router.post('/logreg', function(req,res,next){
+router.post('/logreg', function(req, res, next) {
   var username = req.body.username
   var password = req.body.password
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+      if(user.checkPassword(password)){
+        req.session.user = user
+        res.redirect('/')
+      } else {
+        res.render('logreg', {title: 'Вход', error: 'Пароль не верный'})
+      }
+    } else {
+      var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user
+                res.redirect('/')
+            })     
+    }
+})
 });
 
 module.exports = router;
